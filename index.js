0cf0634cd18e2fcd2bb5ea24da9fd1a24c54a2ca
Pedro Cruz <pedro.cruz.seguros@gmail.com>
2024-10-03 18:55:44 +0100

    make all Games page work + design friendly

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express"
+import axios from "axios"
 
 const app = express()
 const port = 3000
+const APIurl = "https://www.freetogame.com/api"
 
 // Static Files
 app.use(express.static("public"))
@@ -10,6 +12,19 @@ app.use(express.static("public"))
 // Home Page route
 app.get("/", (req, res) => {
     res.render("index.ejs")
+})
+
+// "View All games" route
+app.get("/games", async (req, res) => {
+    try {
+        const allGames = await axios.get(APIurl + "/games")
+        res.render("games.ejs", { articles: allGames.data })        
+        
+    } catch (error) {
+        console.log(error.response.data)
+        console.log(error.data.status)
+        console.log(error.message)       
+    }
 })
 
 
