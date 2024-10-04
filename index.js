4259ec9ca3e90604441050896c7f5649a541a56f
Pedro Cruz <pedro.cruz.seguros@gmail.com>
2024-10-04 19:49:35 +0100

    make individual game page work, opening more details about the game. Style the game_details page a bit.

--- index.js
+++ index.js
@@ -17,15 +17,28 @@ app.get("/", (req, res) => {
     res.render("index.ejs")
 })
 
-// "View All games" and filter by genre route
+// "View All games", "filter by genre" and "game details" route
 
-//if there is a category on the request, it should load a different page, if not show all games
+//if there is a category on the request, it should load "filter by genre page,
+// if there is an id on the request, it should load "game details" page,
+// if not, show all games
 
 app.get("/games", async (req, res) => {
 
     const genreSelected = req.query.category; //gets the query category of the request aka genre
-    
-    if (genreSelected){ //if there is any genre in the request
+    const idOfGameSelected = req.query.id; //gets the query id of the request (in this case when click on any game card)
+
+    if (idOfGameSelected) { // if there is any id on the request
+        try {
+            const gameSelected = await axios.get(APIurl + "/game?id=" + idOfGameSelected)
+            res.render("game_details.ejs", { articles: gameSelected.data})
+
+        } catch (error) {
+            console.log(error.response.data)
+            console.log(error.data.status)
+            console.log(error.message)
+        }        
+    } else if (genreSelected){ //if there is any genre in the request
         try {
             const genreGames = await axios.get(APIurl + "/games?category=" + genreSelected)
             res.render("genre_filter.ejs", { articles: genreGames.data })
@@ -47,6 +60,7 @@ app.get("/games", async (req, res) => {
             console.log(error.message)       
         }
     }
+    
 })
 
 
