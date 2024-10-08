dc9b8f02076eb9ab80e91c563530ea2cdf67efc7
Pedro Cruz <pedro.cruz.seguros@gmail.com>
2024-10-08 19:53:25 +0100

    finish homepage. Make all 'sort by' and filter buttons work properly

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ app.use(express.static("public"))
 
 
 // Home Page route
-app.get("/", async (req, res) => { // app will get the full length of total games available to be ready to show the random game button
+app.get("/", async (req, res) => { // get the full length of total games available to be ready to show the random game button
 
     const totalGames = await axios.get(APIurl + "/games")
     
     var randomPosition = Math.floor(Math.random() * totalGames.data.length); // Stores a random position on the array of objects   
-    var randomId = JSON.stringify(totalGames.data[randomPosition].id) // gets the id value in the position of the random number
-    res.render("index.ejs", { randomId: randomId})  
+    var getRandomId = JSON.stringify(totalGames.data[randomPosition].id) // gets the id value in the position of the random number
+    res.render("index.ejs", { randomId: getRandomId})  
 })
 
 // "View All games", "filter by genre" and "game details" route
@@ -31,7 +31,9 @@ app.get("/games", async (req, res) => {
 
     const genreSelected = req.query.category; //gets the query category of the request aka genre
     const idOfGameSelected = req.query.id; //gets the query id of the request (in this case when click on any game card)
-
+    const sortBy = req.query["sort-by"]; // gets the query sort-by if exists (only in "sort by" buttons)
+    const filterByPlatform = req.query.platform; // gets the query platform if exists (only in "sort by" buttons)
+    
 
     if (idOfGameSelected) { // if there is any id on the request
         try {
@@ -40,18 +42,68 @@ app.get("/games", async (req, res) => {
             res.render("game_details.ejs", { articles: gameSelected.data})
 
         } catch (error) {
-            console.log(error.response.data)
             console.log(error.message)
             
         }        
+    } else if (genreSelected && sortBy){ //if there is a "sort by" and a genre filter
+        try {
+            const multipleSorts = await axios.get(APIurl + "/games?category=" + genreSelected + "&sort-by=" + sortBy)
+            console.log(genreSelected);
+            console.log(sortBy);
+            
+            res.render("genre_filter.ejs", {
+                articles: multipleSorts.data,
+                category: genreSelected,
+            })
+            
+        } catch (error) {
+            console.log(error.message)
+            console.log(error.data)
+        }
+
+    } else if (genreSelected && filterByPlatform){ //if there is a genre filter and a platform filter
+        try {
+            const multipleSorts = await axios.get(APIurl + "/games?category=" + genreSelected + "&platform=" + filterByPlatform)
+            console.log(genreSelected);
+            console.log(filterByPlatform);
+            
+            res.render("genre_filter.ejs", {
+                articles: multipleSorts.data,
+                category: genreSelected,
+            })
+            
+        } catch (error) {
+            console.log(error.message)
+            console.log(error.data)
+        }
+
     } else if (genreSelected){ //if there is any genre in the request
         try {
             const genreGames = await axios.get(APIurl + "/games?category=" + genreSelected)
-            res.render("genre_filter.ejs", { articles: genreGames.data })
+            res.render("genre_filter.ejs", {
+                articles: genreGames.data,
+                category: genreSelected,
+             })
             
         } catch (error) {
-            console.log(error.response.data)
-            console.log(error.data.status)
+            console.log(error.message)
+        }
+
+    } else if (sortBy){ //if there is a "sort by" in the request
+        try {
+            const sortedBy = await axios.get(APIurl + "/games?sort-by=" + sortBy)
+            res.render("games.ejs", { articles: sortedBy.data })
+            
+        } catch (error) {
+            console.log(error.message)
+        }
+
+    } else if (filterByPlatform){ //if there is a "sort by PLATFORM" in the request
+        try {
+            const sortedBy = await axios.get(APIurl + "/games?platform=" + filterByPlatform)
+            res.render("games.ejs", { articles: sortedBy.data })
+            
+        } catch (error) {
             console.log(error.message)
         }
 
@@ -61,8 +113,6 @@ app.get("/games", async (req, res) => {
             res.render("games.ejs", { articles: allGames.data })        
             
         } catch (error) {
-            console.log(error.response.data)
-            console.log(error.data.status)
             console.log(error.message)       
         }
     }
